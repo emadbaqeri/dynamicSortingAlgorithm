7a5f90ad4a8a51545da2221164a657cb619d4323
emad <embaqeri1998@gmail.com>
2021-04-14 04:16:08 +0430

    refactoring the algo for sorting numbers

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
+const numbersCollection = new NumbersCollection([10, 3, -5, -1]);
+const sorter = new Sorter(numbersCollection);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 
-console.warn("OUTPUT -> ", sorter.collection);
+console.log("OUTPUT -> ", numbersCollection.data);
