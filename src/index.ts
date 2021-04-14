4667f1f7f40b6c67612b8b9d36925005156913a1
emad <embaqeri1998@gmail.com>
2021-04-14 04:43:20 +0430

    string(characters) sorting feature added to the Sorter Class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharacterCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, -1]);
+const characterCollection = new CharactersCollection("something here as test");
 const sorter = new Sorter(numbersCollection);
+const charSorter = new Sorter(characterCollection);
 
 sorter.sort();
+charSorter.sort();
+
+console.log("charactersCollection OUTPUT->", characterCollection.data);
 
 console.log("OUTPUT -> ", numbersCollection.data);
